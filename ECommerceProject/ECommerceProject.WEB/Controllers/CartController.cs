81806d5093f6db3325160568208d6ada4b7984fc
Melike Aslan <1melike2@gmail.com>
2023-04-02 03:37:07 +0300

    commit

---
+++ ECommerceProject/ECommerceProject.WEB/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.WEB.Controllers
+{
+	public class CartController : Controller
+	{
+		[AllowAnonymous]
+		public IActionResult Cart()
+		{
+			return View();
+		}
+	}
+}
